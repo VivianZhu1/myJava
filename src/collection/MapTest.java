0e96263a7cf22238c37b228d0f30fa1e3d9f8b6b
VivianZhu1 <vivian.zhu@oracle.com>
2018-10-16 18:36:39 +0800

    palindrome pangram angram

--- src/collection/MapTest.java
+++ src/collection/MapTest.java
@@ -1,5 +1,7 @@
 package collection;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class MapTest {
@@ -13,7 +15,29 @@ public class MapTest {
 			System.out.println(entry);
 		}
 		
+		testHashMap();
 
+		
 	}
 
+	private static void testHashMap() {
+		Map<String, Integer> map = new HashMap<>();
+
+		map.put("语文", 1);
+		map.put("数学", 2);
+		map.put("英语", 3);
+		map.put("历史", 4);
+		map.put("政治", 5);
+		map.put("地理", 6);
+		map.put("生物", 7);
+		map.put("化学", 8);
+		
+		for (Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+		
+		int n = 20;
+		System.out.println(n >>> 16);
+
+	}
 }
